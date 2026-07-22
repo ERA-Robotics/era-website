b284aacb83deba783157c9f9eb2e3a092218c8f7
eraroboticsai <jake@erarobotics.ai>
2026-07-22 23:29:37 +1000

    Update Logo.tsx

--- era-robotics-website-v2/components/Logo.tsx
+++ era-robotics-website-v2/components/Logo.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   return (
@@ -13,9 +13,7 @@ export default function Logo() {
         alt="ERA Robotics"
         width={220}
         height={63}
-        sizes="220px"
         priority
-        fetchPriority="high"
         className="logo-image"
       />
     </Link>
