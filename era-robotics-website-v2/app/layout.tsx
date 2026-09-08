f50812d2e43f3849e67a41d9e9df800671f84ebc
damienera <damien@pudurobotics.io>
2026-09-08 16:59:59 +1000

    Add Google Tag Manager GTM-PK53BJ8W

--- era-robotics-website-v2/app/layout.tsx
+++ era-robotics-website-v2/app/layout.tsx
@@ -94,6 +94,15 @@ export default function RootLayout({
     <html lang="en-AU">
       <head>
         <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PK53BJ8W');`,
+          }}
+        />
+        <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-GBV2P1XP8P"
         />
@@ -109,6 +118,14 @@ export default function RootLayout({
         />
       </head>
       <body className="site-body">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PK53BJ8W"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Header />
         {children}
         <Footer />
